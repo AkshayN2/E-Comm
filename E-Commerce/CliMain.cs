1d1d7dbdbc3fde359f6104fe65b95ea9adfa7bbc
AkshayN2 <akshayn2.hexaware@gmail.com>
2020-12-08 17:12:31 +0530

    Folder Organized

---
+++ E-Commerce/CliMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce
+{
+    class CliMain
+    {
+
+
+        void MainMenu()
+        {
+
+            Console.WriteLine("Welcome to E-comm Command Line Application");
+            Console.WriteLine("\n Please Choose an Option");
+            Console.WriteLine("------------------------\n");
+            Console.WriteLine("1.Login");
+            Console.WriteLine("2.Regiter");
+            Console.WriteLine("3.Exit\n");
+            Console.WriteLine("Your option:");
+
+            int option = int.Parse(Console.ReadLine());
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            CliMain main = new CliMain();
+            main.MainMenu();
+
+        }
+    }
+}

26d139c94ca8150b8869583a847765b1008c634e
yaswanth <yaswanthgeddada@gmail.com>
2020-12-08 16:44:31 +0530

    added auth and user classes

---
+++ Auth.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerce_Application
+{
+    class Auth
+    {
+        private bool _isAuth = false;
+
+        public void logIn()
+        {
+            this._isAuth = true;
+        }
+
+        public void logOut()
+        {
+            this._isAuth = false;
+        }
+
+    }
+}
---
+++ user.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce_Application
+{
+    class user
+    {
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public string role { get; set; }
+        public int balance { get; set; }
+        public string password { get; set; }
+    }
+}
